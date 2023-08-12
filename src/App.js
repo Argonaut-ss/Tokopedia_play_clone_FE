44642a199b5b719b85dd1e554ac975243d6ef697
Argonaut <samysyafta@gmail.com>
2023-08-12 16:46:51 +0700

    style: video detail

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import CardVideo from "./components/CarVideo/CardVideo";
 import CardProduct from "./components/CardProduct/CardProduct";
 import Button from "./components/Button/Button";
+import VideoDetail from "./views/VideoDetail/VideoDetail";
 
 function App() {
   return (
     <div className="App">
-      <CardProduct />
+      <VideoDetail />
     </div>
   );
 }
